3b1caf11eaef606f5b37be7fa7dda2741630a0da
danilolopesabreu <danilo.bcc@gmail.com>
2019-04-11 01:31:33 -0300

    template dinamico com marko

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -6,7 +6,19 @@ module.exports = (app) => {
     
     app.get('/livros', (req, resp) => {
         resp.marko(
-            require('../views/livros/lista/lista.marko')
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros:[
+                    {
+                        id:1,
+                        titulo:'Fundamentos do node'
+                    },
+                    {
+                        id:2,
+                        titulo:'Fundamentos de marko'
+                    }
+                ]
+            }
         );
     });
 }
