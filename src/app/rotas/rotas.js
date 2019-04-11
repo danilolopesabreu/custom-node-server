a4b0001f50a60c25458c3b522d2ae14715efd704
danilolopesabreu <danilo.bcc@gmail.com>
2019-04-11 01:21:45 -0300

    configurando marko.js

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,10 +1,12 @@
 module.exports = (app) => {
 
     app.get('/', (req, resp) => {
-        resp.send(`raiz`);
+        resp.send(`raiz blahhhh`);
     });
     
     app.get('/livros', (req, resp) => {
-        resp.send(`livros`);
+        resp.marko(
+            require('../views/livros/lista/lista.marko')
+        );
     });
 }
